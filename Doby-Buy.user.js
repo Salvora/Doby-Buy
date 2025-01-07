e29d8f8c61186c1b2430cf35fd6ade400320a808
Salvora <6360098+Salvora@users.noreply.github.com>
2025-01-07 22:33:54 +0300

    increment version

--- Doby-Buy.user.js
+++ Doby-Buy.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Doby-Buy
 // @namespace   https://github.com/Salvora
-// @version     1.2.1
+// @version     1.2.2
 // @grant       GM_addStyle
 // @grant       GM_getResourceText
 // @grant       GM_setValue
